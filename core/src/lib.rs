f53919388ab94cb03ca55046317463dc5bd3a1f5
Daniel Collin <daniel@collin.com>
2022-04-15 20:13:14 +0200

    WIP on core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,43 @@
-pub fn do_stuff() {
-    services::foobar();
+use anyhow::Result;
+use log::{error, LevelFilter, Log, SetLoggerError};
+
+pub struct Core {
+    pub dummy: u32,
+}
+
+impl Core {
+    pub fn new() -> Core {
+        Core { dummy: 0 }
+    }
+
+    pub fn update(&mut self) {}
+}
+
+#[no_mangle]
+pub fn core_create() -> *mut Core {
+    let core = Box::leak(Box::new(Core::new()));
+    error!("core create");
+    core as *mut Core
+}
+
+#[no_mangle]
+pub fn core_destroy(core: *mut Core, _prepare_reload: bool) {
+    let _ = unsafe { Box::from_raw(core) };
+    error!("core destroy");
+}
+
+#[no_mangle]
+pub fn core_update(core: *mut Core) {
+    let core: &mut Core = unsafe { &mut *core };
+    error!("created update");
+    core.update();
+}
+
+#[no_mangle]
+pub fn core_setup_logger(
+    logger: &'static dyn Log,
+    level: LevelFilter,
+) -> Result<(), SetLoggerError> {
+    log::set_max_level(level);
+    log::set_logger(logger)
 }
--- services/src/log.rs
+++ services/src/log.rs
@@ -1,3 +1,15 @@
-pub struct Log {
-    dummy: u32,
+use std::fs::File;
+use std::sync::{Arc, Mutex};
+/*
+
+struct SharedState {
+    file_output: File,
 }
+
+struct LogState {
+    state: Arc<Mutex<SharedState>>,
+    name: String,
+}
+*/
+
+pub struct Log {}
